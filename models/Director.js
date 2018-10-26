d01be79272cb4feb556726679de81a29a19b35c4
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-10-26 14:10:37 -0400

    completed actor and director models

--- models/Director.js
+++ models/Director.js
@@ -1,2 +1,14 @@
 const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
+
+const Director = new Schema({
+    name: String,
+    bio: String,
+    img: String,
+    movies: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Movie'
+    }]
+})
+
+module.exports = mongoose.model('Director', Director)
