73e12daec7e06cf69cdf347ed671f74d78219cbc
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-10-26 14:05:09 -0400

    completed movie model

--- models/Movie.js
+++ models/Movie.js
@@ -1,2 +1,19 @@
 const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
+
+const Movie = new Schema({
+    name: String,
+    description: String,
+    img: String,
+    rating: Number,
+    director: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Director'
+    }],
+    actors: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Actor'
+    }]
+})
+
+module.exports = mongoose.model('Movie', Movie)
