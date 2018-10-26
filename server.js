ffaa4e629703566fc04ee66eac5143918502b954
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-10-26 13:49:48 -0400

    connection, server, and node modules installed

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+const routes = require('./routes/index')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'hbs')
+
+app.use(express.static(__dirname + '/public'))
+
+app.use('/', routes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+  })
